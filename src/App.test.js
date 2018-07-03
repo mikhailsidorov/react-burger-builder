9f2519fb2d7ad8a4745ca4f965680836ea64355b
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2018-07-03 23:04:01 +1200

    Add redux store to tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 import App from './App';
+import reducer from './store/reducer'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  const store = createStore(reducer)
   ReactDOM.render((
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
+    <Provider store={store}>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </Provider>
   ), div);
   ReactDOM.unmountComponentAtNode(div);
 });
