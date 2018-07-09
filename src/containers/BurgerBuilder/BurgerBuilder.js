2b021464099d660ab27a5baae81429278a7b3d4b
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2018-07-10 00:02:26 +1200

    Add tests

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import Spinner from '../../components/UI/Spinner/Spinner'
 import * as actions from '../../store/actions/index'
 import axios from '../../axios-orders'
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   state = {
     purchasing: false
   }
